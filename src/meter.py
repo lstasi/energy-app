1a4c579fa3f9b90e509f4f35eddbe4968825dcb7
Leandro Stasi <leandro.stasi@gmail.com>
2025-01-13 23:19:24 +0100

    Update bar

--- src/meter.py
+++ src/meter.py
@@ -12,6 +12,8 @@ class PowerMeter:
         curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.curs_set(0)
         self.max_power = 5000
+        # Set terminal size for 480x320 display (approximately 40x15 characters)
+        print('\x1b[8;15;40t')
 
     def get_power(self):
         try:
@@ -23,28 +25,35 @@ class PowerMeter:
 
     def draw_bar(self, power):
         height, width = self.stdscr.getmaxyx()
-        bar_width = width - 4
+        bar_width = width - 6  # Increase usable width
         power_ratio = power / self.max_power
         filled_width = int(bar_width * power_ratio)
         
-        # Choose color based on power level
-        if power_ratio < 0.6:
-            color = curses.color_pair(1)
-        elif power_ratio < 0.8:
-            color = curses.color_pair(2)
-        else:
-            color = curses.color_pair(3)
-
-        # Draw power bar
-        self.stdscr.addstr(1, 2, f"[{'#' * filled_width}{' ' * (bar_width - filled_width)}]", color)
+        # Draw title and border
+        self.stdscr.box()
+        title = "Power Monitor"
+        self.stdscr.addstr(0, (width - len(title)) // 2, title)
         
-        # Format power value similar to index.html
+        # Draw multiple power bars for better visualization
+        for i in range(6):
+            if power_ratio < 0.6:
+                color = curses.color_pair(1)
+            elif power_ratio < 0.8:
+                color = curses.color_pair(2)
+            else:
+                color = curses.color_pair(3)
+            self.stdscr.addstr(i+2, 3, f"{'█' * filled_width}{' ' * (bar_width - filled_width)}", color)
+        
+        # Format and center power value
         if power >= 1000:
             power_str = f"{power/1000:.2f} kW / {self.max_power/1000:.2f} kW"
         else:
             power_str = f"{int(power)} W / {self.max_power} W"
         
-        self.stdscr.addstr(3, 2, power_str, color)
+        # Add timestamp and centered power value
+        timestamp = time.strftime("%H:%M:%S")
+        self.stdscr.addstr(10, (width - len(power_str)) // 2, power_str, color)
+        self.stdscr.addstr(height-2, 2, f"Updated: {timestamp}")
 
     def run(self):
         try:
